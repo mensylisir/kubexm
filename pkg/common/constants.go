3c9b7e2bf4c8e488a0e3a016cd9344a033d88ba0
mensylisir <710080675@qq.com>
2025-07-09 16:52:27 +0800

    update

--- pkg/common/constants.go
+++ pkg/common/constants.go
@@ -1,6 +1,9 @@
 package common
 
-import "time"
+import (
+	"os"
+	"time"
+)
 
 // This file contains a minimal set of constants to allow pkg/config and pkg/util/validation to compile.
 // Other constants that were previously here and potentially causing redeclaration errors due to
@@ -33,63 +36,47 @@ const (
 // const ValidChartVersionRegexString = ` + "`^v?([0-9]+)(\\.[0-9]+){0,2}$`" + `
 // const DomainValidationRegexString = ` + "`^([a-zA-Z0-9]([a-zA-Z0-9\\-]{0,61}[a-zA-Z0-9])?\\.)*([a-zA-Z0-9]([a-zA-Z0-9\\-]{0,61}[a-zA-Z0-9])?)$`" + `
 
-const (
-	RoleMaster         = "master"
-	RoleWorker         = "worker"
-	RoleEtcd           = "etcd"
-	RoleLoadBalancer   = "loadbalancer"
-	RoleStorage        = "storage"
-	RoleRegistry       = "registry"
-)
-
-const (
-	TaintEffectNoSchedule       = "NoSchedule"
-	TaintEffectPreferNoSchedule = "PreferNoSchedule"
-	TaintEffectNoExecute        = "NoExecute"
-)
-
 // Minimal set of other constants that might be directly or indirectly needed by validation logic
 // or default setting logic in cluster_types.go.
 const (
-	DefaultK8sVersion = "v1.28.2" // Example, as used in some SetDefaults
-    DefaultImageRegistry = "registry.k8s.io"
-    PauseImageName       = "pause"
-    DefaultEtcdVersion           = "3.5.10-0"
-	DefaultCoreDNSVersion        = "v1.10.1"
-	DefaultContainerdVersion     = "1.7.11"
-    DefaultKeepalivedAuthPass          = "kxm_pass"
-    DefaultHAProxyMode                 = "tcp"
-	DefaultHAProxyAlgorithm            = "roundrobin"
-    DefaultKubeletHairpinMode          = "promiscuous-bridge"
-    DefaultLocalRegistryDataDir        = "/var/lib/registry"
-    DefaultRemoteWorkDir      		   = "/tmp/kubexms_work"
-	DefaultWorkDirName                 = ".kubexm"
-	ControlNodeHostName                = "kubexm-control-node"
-	ContainerdSocketPath               = "unix:///run/containerd/containerd.sock"
-	DockerSocketPath                   = "unix:///var/run/docker.sock"
-	CriDockerdSocketPath               = "/var/run/cri-dockerd.sock"
-	KernelModuleBrNetfilter            = "br_netfilter"
-	KernelModuleIpvs                   = "ip_vs"
-	DefaultMinCPUCores                 = 2
-	DefaultMinMemoryMB                 = uint64(2048)
-	CgroupDriverSystemd                = "systemd"
-	CgroupDriverCgroupfs               = "cgroupfs"
-	KubeProxyModeIPTables              = "iptables"
-	KubeProxyModeIPVS                  = "ipvs"
-	CNICalico                          = "calico"
-	CNIFlannel                         = "flannel"
-	CNICilium                          = "cilium"
-	CNIMultus                          = "multus"
-	CNIKubeOvn                         = "kube-ovn"
-	CNIHybridnet                       = "hybridnet"
-	InternalLBTypeHAProxy              = "haproxy"
-	InternalLBTypeNginx                = "nginx"
-	InternalLBTypeKubeVIP              = "kube-vip"
-	ExternalLBTypeExternal             = "external"
-	RuntimeDocker                      = "docker"
-	RuntimeContainerd                  = "containerd"
-	RuntimeCRIO                        = "cri-o"
-	RuntimeIsula                       = "isula"
+	DefaultK8sVersion           = "v1.28.2" // Example, as used in some SetDefaults
+	DefaultImageRegistry        = "registry.k8s.io"
+	PauseImageName              = "pause"
+	DefaultEtcdVersion          = "3.5.10-0"
+	DefaultCoreDNSVersion       = "v1.10.1"
+	DefaultContainerdVersion    = "1.7.11"
+	DefaultKeepalivedAuthPass   = "kxm_pass"
+	DefaultHAProxyMode          = "tcp"
+	DefaultHAProxyAlgorithm     = "roundrobin"
+	DefaultKubeletHairpinMode   = "promiscuous-bridge"
+	DefaultLocalRegistryDataDir = "/var/lib/registry"
+	DefaultRemoteWorkDir        = "/tmp/kubexms_work"
+	DefaultWorkDirName          = ".kubexm"
+	ContainerdSocketPath        = "unix:///run/containerd/containerd.sock"
+	DockerSocketPath            = "unix:///var/run/docker.sock"
+	CriDockerdSocketPath        = "/var/run/cri-dockerd.sock"
+	KernelModuleBrNetfilter     = "br_netfilter"
+	KernelModuleIpvs            = "ip_vs"
+	DefaultMinCPUCores          = 2
+	DefaultMinMemoryMB          = uint64(2048)
+	CgroupDriverSystemd         = "systemd"
+	CgroupDriverCgroupfs        = "cgroupfs"
+	KubeProxyModeIPTables       = "iptables"
+	KubeProxyModeIPVS           = "ipvs"
+	CNICalico                   = "calico"
+	CNIFlannel                  = "flannel"
+	CNICilium                   = "cilium"
+	CNIMultus                   = "multus"
+	CNIKubeOvn                  = "kube-ovn"
+	CNIHybridnet                = "hybridnet"
+	InternalLBTypeHAProxy       = "haproxy"
+	InternalLBTypeNginx         = "nginx"
+	InternalLBTypeKubeVIP       = "kube-vip"
+	ExternalLBTypeExternal      = "external"
+	RuntimeDocker               = "docker"
+	RuntimeContainerd           = "containerd"
+	RuntimeCRIO                 = "cri-o"
+	RuntimeIsula                = "isula"
 )
 
 // File Permissions
