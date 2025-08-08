e34fdd5805745b31c855dca0b9ed8778e1726ff3
mensylisir <710080675@qq.com>
2025-08-08 15:51:10 +0800

    优化longhorn部署

--- pkg/step/helm/install_helm.go
+++ pkg/step/helm/install_helm.go
@@ -4,14 +4,15 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
-	"strings"
 	"time"
 
 	"github.com/mensylisir/kubexm/pkg/common"
 	"github.com/mensylisir/kubexm/pkg/runtime"
 	"github.com/mensylisir/kubexm/pkg/spec"
 	"github.com/mensylisir/kubexm/pkg/step"
+	"github.com/mensylisir/kubexm/pkg/step/helpers"
 	"github.com/mensylisir/kubexm/pkg/step/helpers/bom/binary"
+	"github.com/pkg/errors"
 )
 
 type InstallHelmStep struct {
@@ -25,14 +26,21 @@ type InstallHelmStepBuilder struct {
 }
 
 func NewInstallHelmStepBuilder(ctx runtime.Context, instanceName string) *InstallHelmStepBuilder {
+	provider := binary.NewBinaryProvider(&ctx)
+	const representativeArch = "amd64"
+	binaryInfo, err := provider.GetBinary(binary.ComponentHelm, representativeArch)
+	if err != nil || binaryInfo == nil {
+		return nil
+	}
+
 	s := &InstallHelmStep{
 		InstallPath: common.DefaultBinDir,
 		Permission:  "0755",
 	}
 
 	s.Base.Meta.Name = instanceName
-	s.Base.Meta.Description = fmt.Sprintf("[%s]>>Install helm binary", s.Base.Meta.Name)
-	s.Base.Sudo = false
+	s.Base.Meta.Description = fmt.Sprintf("[%s]>>Install helm binary to system path", s.Base.Meta.Name)
+	s.Base.Sudo = true
 	s.Base.IgnoreError = false
 	s.Base.Timeout = 2 * time.Minute
 
@@ -58,7 +66,7 @@ func (s *InstallHelmStep) Meta() *spec.StepMeta {
 	return &s.Base.Meta
 }
 
-func (s *InstallHelmStep) getLocalExtractedPath(ctx runtime.ExecutionContext) (string, error) {
+func (s *InstallHelmStep) getLocalSourcePath(ctx runtime.ExecutionContext) (string, error) {
 	provider := binary.NewBinaryProvider(ctx)
 	arch := ctx.GetHost().GetArch()
 	binaryInfo, err := provider.GetBinary(binary.ComponentHelm, arch)
@@ -68,11 +76,12 @@ func (s *InstallHelmStep) getLocalExtractedPath(ctx runtime.ExecutionContext) (s
 	if binaryInfo == nil {
 		return "", fmt.Errorf("helm is unexpectedly disabled for arch %s", arch)
 	}
-
 	s.Base.Meta.Description = fmt.Sprintf("[%s]>>Install helm binary (version %s)", s.Base.Meta.Name, binaryInfo.Version)
 
-	destDirName := strings.TrimSuffix(binaryInfo.FileName(), ".tar.gz")
-	return filepath.Join(ctx.GetExtractDir(), destDirName), nil
+	extractedDir := filepath.Dir(binaryInfo.FilePath())
+	innerDir := fmt.Sprintf("linux-%s", arch)
+
+	return filepath.Join(extractedDir, innerDir, "helm"), nil
 }
 
 func (s *InstallHelmStep) getRemoteTargetPath() string {
@@ -81,24 +90,30 @@ func (s *InstallHelmStep) getRemoteTargetPath() string {
 
 func (s *InstallHelmStep) Precheck(ctx runtime.ExecutionContext) (isDone bool, err error) {
 	logger := ctx.GetLogger().With("step", s.Base.Meta.Name, "host", ctx.GetHost().GetName(), "phase", "Precheck")
-	runner := ctx.GetRunner()
-	conn, err := ctx.GetCurrentHostConnector()
-	if err != nil {
-		return false, err
-	}
 
-	targetPath := s.getRemoteTargetPath()
-	exists, err := runner.Exists(ctx.GoContext(), conn, targetPath)
+	localSourcePath, err := s.getLocalSourcePath(ctx)
 	if err != nil {
-		return false, fmt.Errorf("failed to check for file '%s' on host %s: %w", targetPath, ctx.GetHost().GetName(), err)
-	}
-	if exists {
-		logger.Infof("Target file '%s' already exists. Step is done.", targetPath)
+		logger.Infof("Helm is not required for this host (arch: %s), skipping.", ctx.GetHost().GetArch())
 		return true, nil
 	}
 
-	logger.Infof("Target file '%s' does not exist. Installation is required.", targetPath)
-	return false, nil
+	if _, err := os.Stat(localSourcePath); os.IsNotExist(err) {
+		return false, errors.Wrapf(err, "local source file '%s' not found, ensure extract step ran successfully", localSourcePath)
+	}
+
+	targetPath := s.getRemoteTargetPath()
+	isDone, err = helpers.CheckRemoteFileIntegrity(ctx, localSourcePath, targetPath, s.Sudo)
+	if err != nil {
+		return false, errors.Wrapf(err, "failed to check remote file integrity for %s", targetPath)
+	}
+
+	if isDone {
+		logger.Infof("Target file '%s' already exists and is up-to-date. Step is done.", targetPath)
+	} else {
+		logger.Infof("Target file '%s' is missing or outdated. Installation is required.", targetPath)
+	}
+
+	return isDone, nil
 }
 
 func (s *InstallHelmStep) Run(ctx runtime.ExecutionContext) error {
@@ -109,48 +124,39 @@ func (s *InstallHelmStep) Run(ctx runtime.ExecutionContext) error {
 		return err
 	}
 
-	localExtractedPath, err := s.getLocalExtractedPath(ctx)
+	localSourcePath, err := s.getLocalSourcePath(ctx)
 	if err != nil {
 		return err
 	}
 
-	arch := ctx.GetHost().GetArch()
-	osName := "linux"
-	localSourcePath := filepath.Join(localExtractedPath, fmt.Sprintf("%s-%s", osName, arch), "helm")
-
-	if _, err := os.Stat(localSourcePath); os.IsNotExist(err) {
-		return fmt.Errorf("local source file '%s' not found, ensure extract step ran successfully", localSourcePath)
-	}
-
 	if err := runner.Mkdirp(ctx.GoContext(), conn, s.InstallPath, "0755", s.Sudo); err != nil {
-		return fmt.Errorf("failed to create remote install directory '%s': %w", s.InstallPath, err)
+		return errors.Wrapf(err, "failed to create remote install directory '%s'", s.InstallPath)
 	}
 
-	remoteUploadTmpDir := filepath.Join(ctx.GetUploadDir(), fmt.Sprintf("helm-%d", time.Now().UnixNano()))
-	if err := runner.Mkdirp(ctx.GoContext(), conn, remoteUploadTmpDir, "0755", false); err != nil {
-		return fmt.Errorf("failed to create remote upload directory '%s': %w", remoteUploadTmpDir, err)
+	remoteUploadTmpDir := filepath.Join(ctx.GetUploadDir(), fmt.Sprintf("helm-install-%d", time.Now().UnixNano()))
+	if err := runner.Mkdirp(ctx.GoContext(), conn, remoteUploadTmpDir, "0755", false); err != nil { // No sudo for tmp dir
+		return errors.Wrapf(err, "failed to create remote upload directory '%s'", remoteUploadTmpDir)
 	}
 	defer func() {
 		_ = runner.Remove(ctx.GoContext(), conn, remoteUploadTmpDir, false, true)
 	}()
 
-	targetPath := s.getRemoteTargetPath()
 	remoteTempPath := filepath.Join(remoteUploadTmpDir, "helm")
-	logger.Infof("Uploading helm to %s:%s", ctx.GetHost().GetName(), remoteTempPath)
-
+	logger.Infof("Uploading helm to temporary path %s:%s", ctx.GetHost().GetName(), remoteTempPath)
 	if err := runner.Upload(ctx.GoContext(), conn, localSourcePath, remoteTempPath, false); err != nil {
-		return fmt.Errorf("failed to upload '%s' to '%s': %w", localSourcePath, remoteTempPath, err)
+		return errors.Wrapf(err, "failed to upload '%s' to '%s'", localSourcePath, remoteTempPath)
 	}
 
+	targetPath := s.getRemoteTargetPath()
 	moveCmd := fmt.Sprintf("mv %s %s", remoteTempPath, targetPath)
-	logger.Infof("Moving file to %s on remote host", targetPath)
+	logger.Infof("Moving file to final destination %s on remote host", targetPath)
 	if _, err := runner.Run(ctx.GoContext(), conn, moveCmd, s.Sudo); err != nil {
-		return fmt.Errorf("failed to move file to '%s': %w", targetPath, err)
+		return errors.Wrapf(err, "failed to move file to '%s'", targetPath)
 	}
 
 	logger.Infof("Setting permissions for %s to %s", targetPath, s.Permission)
 	if err := runner.Chmod(ctx.GoContext(), conn, targetPath, s.Permission, s.Sudo); err != nil {
-		return fmt.Errorf("failed to set permission on '%s': %w", targetPath, err)
+		return errors.Wrapf(err, "failed to set permission on '%s'", targetPath)
 	}
 
 	logger.Infof("Successfully installed helm to %s", targetPath)
@@ -162,6 +168,7 @@ func (s *InstallHelmStep) Rollback(ctx runtime.ExecutionContext) error {
 	runner := ctx.GetRunner()
 	conn, err := ctx.GetCurrentHostConnector()
 	if err != nil {
+		logger.Errorf("Failed to get connector for rollback: %v", err)
 		return nil
 	}
 
